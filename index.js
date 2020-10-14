fefd11cd0b5173fdf2418f0da731d62465a00ef3
Leo Porto <leonardoreisporto@gmail.com>
2020-10-14 08:14:36 -0300

    alteracoes necessarias para envio de evento do server para o front - const para let

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const electron = require('electron')
 const sizeOf = require('image-size')
 const { app, BrowserWindow, ipcMain } = electron
 
-
+//mudamos a variavel de const para let para podermos fazer uma atribuicao a ela mais tarde
 let janela_principal
 
 app.on('ready', () => {
