6bf7df66ad3ca41038a76cbbd196e64078358807
Leo Porto <leonardoreisporto@gmail.com>
2020-10-14 13:19:54 -0300

    criando submenu

--- app.js
+++ app.js
@@ -41,7 +41,28 @@ ipcMain.on('teste', (e, data) => {
 
 let menuTemplate = [
     {
-        label: 'File'
+        label: 'File',
+        submenu: [
+            {
+                label: 'New File'
+            },
+
+            {
+                label: 'New Window'
+            },
+
+            {
+                label: 'Open File'
+            },
+
+            {
+                label: 'Open Folder'
+            },
+            {
+
+                label: 'Open Workspace'
+            }
+        ]
     },
 
     {
