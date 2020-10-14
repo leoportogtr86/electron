5a7d14aa176234dda320b76efef629547ccac0ce
Leo Porto <leonardoreisporto@gmail.com>
2020-10-14 07:19:26 -0300

    permitindo a integracao com o node

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const electron = require('electron')
-
+const sizeOf = require('image-size')
 const { app, BrowserWindow } = electron
+
 
 
 
 app.on('ready', () => {
 
 
-    const janela_principal = new BrowserWindow({})
+    const janela_principal = new BrowserWindow({
+
+        webPreferences: {
+
+            nodeIntegration: true
+        }
+    })
     janela_principal.loadURL(`file://${__dirname}/index.html`)
 
+
+
     //file://${__dirname}/index.html
+
+
 
 
 
