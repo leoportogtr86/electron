f080af0c60fdb35324a178ebb1e4d0e2d2822e5e
Leo Porto <leonardoreisporto@gmail.com>
2020-10-14 06:49:45 -0300

    carregando arquivos via input file

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ app.on('ready', () => {
     const janela_principal = new BrowserWindow({})
     janela_principal.loadURL(`file://${__dirname}/index.html`)
 
+    //file://${__dirname}/index.html
+
 
 
 })
