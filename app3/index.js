d1ebf5de79e4ee6d4cf5eea414941901a7c9f0d3
Leo Porto <leonardoreisporto@gmail.com>
2020-10-19 14:58:56 -0300

    criando o menu de contexto para o icone da bandeja

--- app3/index.js
+++ app3/index.js
@@ -1,5 +1,5 @@
 const electron = require('electron')
-const { app, Tray } = electron
+const { app, Tray, Menu } = electron
 
 
 app.on('ready', () => {
@@ -13,5 +13,36 @@ app.on('ready', () => {
         console.log('ícone foi clicado')
     })
 
+    const contextMenu = Menu.buildFromTemplate(menuTemplate)
+    tray.setContextMenu(contextMenu)
+
 
 })
+
+const menuTemplate = [
+
+    {
+
+        label: 'Label 1',
+        click() {
+
+            console.log('Você clicou na opção 1')
+        }
+    },
+    {
+
+        label: 'Label 2',
+        click() {
+
+            console.log('Você clicou na opção 2')
+        }
+    },
+    {
+
+        label: 'Label 3',
+        click() {
+
+            console.log('Você clicou na opção 3')
+        }
+    }
+]
